c0561e9cd9b5a7ebcec773f64abc0ffb58170602
kedarshenoy <akedarshenoy@gmail.com>
2023-08-01 18:31:22 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function opencertificate(name){
+   if(name=="main"){
+    open(src=("./edureka/main.pdf"))
+   }
+   else if(name=="mod1"){
+    open(src=("./edureka/mod1.pdf"))
+   }else if(name=="mod2"){
+    open(src=("./edureka/mod2.pdf"))
+   }else if(name=="mod3"){
+    open(src=("./edureka/mod4.pdf"))
+   }else if(name=="mod4"){
+    open(src=("./edureka/mod3.pdf"))
+   }else if(name=="course"){
+      open(src=("./edureka/course.pdf"))
+     }
+   else{
+    window.alert("not found")
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+
